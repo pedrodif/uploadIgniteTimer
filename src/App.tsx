f43c0b5e2971d74588b7b9256cfd19c9b742aa1f
Pedro Ferreira <pedrodif@icloud.com>
2022-08-04 13:12:06 -0300

    fix: :bug:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 // Packages
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
 // Components
+import { Router } from './Router'
 
 // Styles
 import { GlobalStyle } from './styles/global'
@@ -10,6 +12,9 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/Router.tsx
+++ src/Router.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 
 // Component
 import { Home } from './pages/Home'
+import { History } from './pages/History'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
     </Routes>
   )
 }
