0af03ca1f341cd912a661832794b54277b650648
Pedro Ferreira <pedrodif@icloud.com>
2022-08-04 19:30:11 -0300

    feat: :art:

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,17 @@
+// Packages
+import { Outlet } from 'react-router-dom'
+
+// Components
+import { Header } from '../../components/Header'
+
+// Styled Components
+import { LayoutContainer } from './styles'
+
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}
