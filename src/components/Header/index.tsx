7cca65968faa3e7f472361cd07e062a9c05ba911
Pedro Ferreira <pedrodif@icloud.com>
2022-08-05 12:17:07 -0300

    feat: :art:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,28 @@
+// Packages
+import { NavLink } from 'react-router-dom'
+
+// Styles
+import { HeaderContainer } from './styles'
+
+// Assets
+import logoIgnite from '../../assets/logo-ignite.svg'
+
+// Components
+import { Timer, Scroll } from 'phosphor-react'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
