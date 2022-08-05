a73a7729b035e70d7064cac65029199eafb3d62b
Pedro Ferreira <pedrodif@icloud.com>
2022-08-05 14:07:15 -0300

    feat: :art:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,11 +20,20 @@ export function Home() {
           <label htmlFor="task">Vou tabralhar em</label>
           <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="banana" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
           />
 
           <span>minutos.</span>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -36,6 +36,15 @@ const BaseInput = styled.input`
   font-size: 1.125rem;
   padding: 0 0.5rem;
   color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
 `
 export const TaskInput = styled(BaseInput)`
   flex: 1;
