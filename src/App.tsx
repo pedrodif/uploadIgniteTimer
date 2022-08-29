1d5eb6b9f5365a97695228c9e7582e51866fb2d8
Pedro Ferreira <pedrodif@icloud.com>
2022-08-29 16:20:29 -0300

    feat: :art:

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-// Packages
-// 'import { ThemeProvider } from 'styled-components'
-// import { BrowserRouter } from 'react-router-dom'
+Packages
+'import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
-// // Components
-// import { Router } from './Router'
+// Components
+import { Router } from './Router'
 
-// // Styles
-// import { GlobalStyle } from './styles/global'
-// import { defaultTheme } from './styles/themes/default''
-import { Home } from './home'
+// Styles
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default''
 
 export function App() {
-  return <Home />
-
-  // return (
-  //   <ThemeProvider theme={defaultTheme}>
-  //     <BrowserRouter>
-  //       <Router />
-  //     </BrowserRouter>
-  //     <GlobalStyle />
-  //   </ThemeProvider>
-  // )
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
